c424b6120f3916dd8362e03fcf3460d2fd40116c
Unknown <JonathanILevi@gmail.com>
2019-10-24 11:49:56 -0500

    The main JS code.
    Really just testing code...

---
+++ www-src/packages/main/modules/Main.m.js
@@ -0,0 +1,71 @@
+import {div,Div} from "/modules/Div.m.js";
+import makeLayout from "./Layout.m.js";
+
+function startNetworking() {
+	let ws = new WebSocket("ws://"+document.location.host);
+	
+	ws.addEventListener("open",e=>console.log("Open ",e));
+	ws.addEventListener("error",e=>console.log("Error ",e));
+	ws.addEventListener("message",e=>{
+		console.log("Message ",e)
+		e.data.arrayBuffer().then(d=>{
+			console.log(d);
+		});
+	});
+	ws.addEventListener("close",e=>console.log("Close ",e));
+	
+	window.ws = ws;
+}
+
+function send(value) {
+	value = networkFloat(value,16,true);
+	
+	let builder = new flatbuffers.Builder(1024);
+	Data.Msg.Up.SetThruster.startSetThruster(builder);
+	Data.Msg.Up.SetThruster.addId(builder, 0);
+	Data.Msg.Up.SetThruster.addValue(builder, value);
+	let thruster = Data.Msg.Up.SetThruster.endSetThruster(builder);
+	Data.Msg.Up.UpMsg.startUpMsg(builder);
+	Data.Msg.Up.UpMsg.addContentType(builder, Data.Msg.Up.MsgContent.SetThruster);
+	Data.Msg.Up.UpMsg.addContent(builder, thruster);
+	let msg = Data.Msg.Up.UpMsg.endUpMsg(builder);
+	builder.finish(msg);
+	let buf = builder.asUint8Array();
+	ws.send(buf);
+}
+window.send = send;
+
+function networkFloat(value,bits,signed=true,value100=1) {
+	value = Math.min(value100,Math.max(signed?-value100:0,value));
+	if (signed)
+		value = Math.trunc(value*(Math.pow(2,bits)/2-1));
+	else
+		value = Math.trunc(value*(Math.pow(2,bits)-1));
+	return value;
+}
+
+
+////let [componentsPush,controls] = makeLayout();
+startNetworking();
+
+let iframe = div("iframe");
+document.body.appendChild(iframe);
+iframe.contentDocument.body.parentElement.replaceChild(makeLayout(send), iframe.contentDocument.body);
+////iframe.contentDocument.bodyj.appendChild(controls);
+////document.body.appendChild(iframe);
+////
+////
+////
+////
+////
+////
+////Object.defineProperty(Blob.prototype, 'arrayBuffer', {
+////  configurable: true,
+////  enumerable: true,
+////  writable: true,
+////  value: function arrayBuffer() {
+////    return new Response(this).arrayBuffer();
+////  }
+////});
+
+
