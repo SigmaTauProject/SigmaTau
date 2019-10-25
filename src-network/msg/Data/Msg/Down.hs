1ed963f1d26c2ddd44ffa494209f1329f583552f
Unknown <JonathanILevi@gmail.com>
2019-10-25 12:28:05 -0500

    Add down schema and js & hs shim/build code.

---
+++ src-network/msg/Data/Msg/Down.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Data.Msg.Down (module Msg.Down, Table) where
+
+import Msg.Down
+import FlatBuffers
--- www-src/package-builders/Msg/build.sh
+++ www-src/package-builders/Msg/build.sh
@@ -1,3 +1,4 @@
 trash ./build/*
 flatc --js -o ./build/scripts ../../../schemas/up.fbs
+flatc --js -o ./build/scripts ../../../schemas/down.fbs
 
