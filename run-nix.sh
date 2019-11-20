0ffe9775d1a4c9273d8b517e52efcd45b1323775
Jonathan <JonathanILevi@gmail.com>
2019-11-20 11:51:17 -0600

    Bug fix: run-nix did not recompile the hpack.

--- run-nix.sh
+++ run-nix.sh
@@ -1,3 +1,3 @@
 
-./prebuild.sh && cabal new-run --extra-lib-dirs="${PWD}/world-src/build/" --extra-lib-dirs="${PWD}/dlib"
+./prebuild.sh && hpack && cabal new-run --extra-lib-dirs="${PWD}/world-src/build/" --extra-lib-dirs="${PWD}/dlib"
 ##./prebuild.sh && hpack && cabal new-run --extra-lib-dirs="${PWD}/world-src/build/" 
